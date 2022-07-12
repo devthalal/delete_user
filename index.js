c460a9e98100ed8cdac45e1ac555c62282d7e082
devthalal <thalal.hassan@neoito.com>
2022-07-12 17:58:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const delete_user = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello delete_user`}))
+  res.end()
+  
+}
+
+export default delete_user
