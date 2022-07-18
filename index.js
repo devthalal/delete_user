04213cc1e2b8ef0c8a5974f2839fc8dce814e18b
devthalal <thalal.hassan@neoito.com>
2022-07-18 10:23:31 +0530

    schema updates

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-
+import { shared } from "node-blox-sdk";
+import { getBody } from "./utils.js";
 
 const delete_user = async (req, res) => {
-
   // health check
   if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    res.write(JSON.stringify({ success: true, msg: "Health check success" }));
+    res.end();
   }
 
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello delete_user`}))
-  res.end()
-  
-}
+  // Getting shared prisma client
+  const { prisma } = await shared.getShared();
 
-export default delete_user
+  const data = await getBody(req);
+  const { system_user_id } = data;
+  const dbRes = await prisma.user.delete({
+    where: { system_user_id },
+  });
+
+  res.write(
+    JSON.stringify({
+      success: true,
+      msg: `User deleted succesfully`,
+      data: dbRes,
+    })
+  );
+  res.end();
+};
+
+export default delete_user;
